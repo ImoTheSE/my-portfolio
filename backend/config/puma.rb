ea950329546ee2c9a5764ba85c929f3a595f93db
ImoTheSe <oimoriko@gmail.com>
2025-08-03 19:52:42 +0900

    20250803_backendデプロイ失敗に伴う変更6

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -2,7 +2,6 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count).to_i
 threads min_threads_count, max_threads_count
 
-workers ENV.fetch("WEB_CONCURRENCY", 1).to_i
 
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
