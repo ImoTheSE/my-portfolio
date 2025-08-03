644703410da186f673419bd98c89eccbf7f0b263
ImoTheSe <oimoriko@gmail.com>
2025-08-03 14:58:43 +0900

    20250803_Railsコードを本番環境用に整える（Render用）

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,8 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # Nuxt 側のURL
+    origins 'http://localhost:3001', 'https://your-vercel-url.vercel.app'
+
     resource '*',
              headers: :any,
              methods: %i[get post options delete put patch],
